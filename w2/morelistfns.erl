bcad34a8384cb8dedfd76eee82b1f01a999895a8
Evan Rowley <rowley.evan@gmail.com>
2018-06-07 14:25:03 -0400

    modes/1 in morelistfns.erl needed the logic in the case expression switched around

--- w2/morelistfns.erl
+++ w2/morelistfns.erl
@@ -40,17 +40,16 @@ median( [Num|Nums], Len ) when ( Len rem 2 =:= 1 ) ->
 median( [Num|Nums], Len ) -> 
 	average( [ lists:nth( (Len div 2), [Num|Nums] ), lists:nth( (Len div 2 + 1), [Num|Nums] ) ] ).
 
-% personally, I feel like the modes function is not well defined. If the list contains one number, then we are only supposed to return a list with that number. If the list contains many numbers, then we are to return a list of numbers which appear most frequently. But what are we supposed to do when the list contains all unique numers? Just return the list of unique numbers? After all, the are the numbers which occur most frequently... But they all only occur once, so that doesn't seem like quite a correct answer either. If they are supposed to be returned, then it seems like what this function really should do is just return a list where any duplicated numbers are de-duped, so only unique numbers remain. This is my best guess to what should be done here, so that's how my modes/2 will work:
+% personally, I feel like the modes function question in this lession is not well defined. It's easy to understand that if the list contains one number, then we are only supposed to return a list with that number. It's also easy to understand that if the list contains many numbers, then we are to return a list of numbers which appear most frequently. But what is not clear is what are we supposed to do when the list contains all unique numers? Just return the list of unique numbers? After all, those would be the numbers which occur most frequently... But they all only occur once, so that doesn't seem like quite a correct answer either, becuause this is looking for numbers which occur frequently. If they are supposed to be returned, then it seems like what this function really should do is just return a list where any duplicated numbers are de-duped, so that only unique numbers remain. This is my best guess to what should be done here, so that's how my modes/2 will work:
 
-modes( [] ) -> 
+modes( [] ) -> % output empty list if input list is empty
 	[];
-modes( [X|Xs] ) when ( length(Xs) =:= 0 ) ->
+modes( [X|Xs] ) when ( length(Xs) =:= 0 ) -> % output list with one element if input is list with one element
 	lists:append([],[X]);
-modes( [X|Xs] ) ->
+modes( [X|Xs] ) -> % When the list has more than one element, run a foldr on a list to accumulate (Acc) elements (Elem) which aren't already contained in Acc. Should give a list with only unique elements.
 	lists:foldr(fun(Elem, Acc) ->
 		case lists:member(Elem, Acc) of
-			false -> Acc; % false -> Acc; % Is Elem inside Acc already?
-			true -> [Elem|Acc]%;
-			%{true,Value} -> [Value|Acc]
+			true -> Acc; % false -> Acc;
+			false -> [Elem|Acc]
 		end
 	end, [], [X|Xs]).
